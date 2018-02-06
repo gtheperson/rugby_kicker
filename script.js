0d41d8361f01d0a1eb1dd33d98488fa214964524
gtheperson <34607312+gtheperson@users.noreply.github.com>
2018-02-06 19:21:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -203,15 +203,15 @@ function moveStopScore() {
     }
     if (scoreText == true) {// if score text then print goal
       ctx.fillText("Goal!", 300, 30);
-    } 
+    }
+    if (scoreText != true && missed != true) {
+      oohSound.play();
+    }
   } // if the ball was kicked then reduce shots left
   if (kicked == true && y > goalY + 50 && velY < 0) {// if ball is moving downward and is below the goal turn kick off so ball stops moving and do score stuff
     shots += 1;
     console.log(shots + "shots");
     shotTaken = true;
-    if (scoreText != true && missed != true) {
-      oohSound.play();
-    }
     kicked = false;
   }
 }
